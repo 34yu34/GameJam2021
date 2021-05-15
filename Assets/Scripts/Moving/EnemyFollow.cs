9e7c6a770363f16b9c6428b190a347ecb863a7ee
Audrey Germain <audrey.germain@polymtl.ca>
2021-05-15 09:24:41 -0700

    enemy follow

--- Assets/Scripts/Moving/EnemyFollow.cs
+++ Assets/Scripts/Moving/EnemyFollow.cs
@@ -3,17 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-
+[RequireComponent(typeof(Rigidbody))]
 public class EnemyFollow : MonoBehaviour
 {
     [SerializeField]
     private NavMeshAgent _nav_mesh_agent;
 
     [SerializeField]
-    private Transform _player;
+    private GameObject _player;
 
-    void Update()
+    [SerializeField]
+    private Collider _collider_player;
+
+    enum State { Calm, Chase };
+
+    private State _state = State.Calm;
+
+    void FixedUpdate()
     {
-        _nav_mesh_agent.SetDestination(_player.position);
+        change_state();
+        Debug.Log(_state);
+        switch (_state)
+        {
+            case State.Calm:
+                _nav_mesh_agent.ResetPath();
+                break;
+            case State.Chase:
+                _nav_mesh_agent.SetDestination(_player.transform.position);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void change_state()
+    {
+        switch (_state)
+        {
+            case State.Calm:
+                if (sees_player())
+                {
+                    _state = State.Chase;
+                }
+                break;
+            case State.Chase:
+                if (is_far_from_player())
+                {
+                    _state = State.Calm;
+                }
+                break;
+            default:
+                _state = State.Calm;
+                break;
+            
+
+        }
+    }
+
+    private bool sees_player()
+    {
+        RaycastHit hit;
+        bool ray = Physics.Raycast(transform.position + Vector3.up, _player.transform.position, out hit);
+        Debug.Log(ray);
+        return Vector3.Angle(transform.forward, _player.transform.position - transform.position) < 181 &&
+            ray &&
+            hit.collider == _collider_player;
+    }
+
+    private bool is_far_from_player()
+    {
+        return Vector3.Distance(transform.position, _player.transform.position) > 10f;
     }
 }
