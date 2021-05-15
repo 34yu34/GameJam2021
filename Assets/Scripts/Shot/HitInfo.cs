9dd11a275e249f354ce1f338cd1f25d0d976223f
Billy <billy.bouchard03@gmail.com>
2021-05-14 21:50:03 -0400

    Add shot effect

---
+++ Assets/Scripts/Shot/HitInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct HitInfo
+{
+    public Vector3 HitPosition;
+    public Vector3 Origin;
+    public int Damage;
+}
