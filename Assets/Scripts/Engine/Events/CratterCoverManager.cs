6cb6143c6cfcf932b77c8173b694699eb3906e86
jacob643 <jacobdorais25@hotmail.com>
2021-05-16 06:13:56 -0400

    ongoing bomb-evvent

--- Assets/Scripts/Engine/Events/CratterCoverManager.cs
+++ Assets/Scripts/Engine/Events/CratterCoverManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CratterCoverManager : MonoBehaviour
@@ -8,34 +9,45 @@ public class CratterCoverManager : MonoBehaviour
     
     void Start()
     {
-        foreach(var transform in GetComponentsInChildren<Transform>())
+        foreach(var sphere_collider in GetComponentsInChildren<SphereCollider>())
         {
-            _cratter_cover_list.Add(transform.gameObject);
+            _cratter_cover_list.Add(sphere_collider.gameObject);
         }
     }
 
-    public bool HasCratterLeft()
+    public bool HasCraterLeft()
     {
-        return _cratter_cover_list.Count > 0;
+        return _cratter_cover_list.Any();
     }
 
     public GameObject GetFurthestCratterFrom(Vector3 position)
     {
-        var furthest = _cratter_cover_list[0];
-        var furthest_dist = Vector3.Distance(position, furthest.transform.position + Vector3.up * furthest.transform.localScale.y / 2);
-
-        for(int i = 1; i < _cratter_cover_list.Count; i++)
+        if (!HasCraterLeft())
         {
-            var cratter = _cratter_cover_list[i];
-            var dist = Vector3.Distance(position, cratter.transform.position + Vector3.up * cratter.transform.localScale.y / 2);
+            return null;
+        }
 
-            if(dist > furthest_dist)
+        var furthest = _cratter_cover_list.First();
+        float furthest_dist = find_distance_between(position, furthest.transform);
+
+        foreach (var crater in _cratter_cover_list.Skip(1))
+        {
+            var dist = find_distance_between(position, crater.transform);
+
+            if (dist > furthest_dist)
             {
-                furthest = cratter;
+                furthest = crater;
                 furthest_dist = dist;
             }
         }
 
+        _cratter_cover_list.Remove(furthest);
+
         return furthest;
+    }
+
+    private float find_distance_between(Vector3 position, Transform furthest)
+    {
+        return Vector3.Distance(position, furthest.position + Vector3.up * furthest.localScale.y / 2);
     }
 }
